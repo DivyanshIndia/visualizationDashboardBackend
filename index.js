6a85a12e25a996ea37bdf08f2a160939097a6c6c
DivyanshIndia <thankswe45@gmail.com>
2024-03-30 17:02:00 +0530

    routes opens after connection is made with mongodb atlas

--- index.js
+++ index.js
@@ -31,49 +31,49 @@ client
   .then((client) => {
     db = client.db("crpto_data");
     console.log("Connected to MongoDB");
+
+    app.get("/", (req, res) => {
+      res
+        .status(200)
+        .send("Crypto Data server is connected to your frontend application!");
+    });
+
+    app.get("/btcusd", async (req, res) => {
+      try {
+        const collection = db.collection("BTCUSD");
+        const data = await collection.find({}).toArray();
+        if (data.length === 0) {
+          res.status(404).json({ message: "No data found for BTCUSD" });
+        } else {
+          res.status(200).json(data);
+        }
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.get("/eurusd", async (req, res) => {
+      try {
+        const collection = db.collection("EURUSD");
+        const data = await collection.find({}).toArray();
+        if (data.length === 0) {
+          res.status(404).json({ message: "No data found for EURUSD" });
+        } else {
+          res.status(200).json(data);
+        }
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   })
   .catch((error) => {
     console.error("Connection error:", error);
     process.exit(1);
   });
-
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send("Crypto Data server is connected to your frontend application!");
-});
-
-app.get("/btcusd", async (req, res) => {
-  try {
-    const collection = db.collection("BTCUSD");
-    const data = await collection.find({}).toArray();
-    if (data.length === 0) {
-      res.status(404).json({ message: "No data found for BTCUSD" });
-    } else {
-      res.status(200).json(data);
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.get("/eurusd", async (req, res) => {
-  try {
-    const collection = db.collection("EURUSD");
-    const data = await collection.find({}).toArray();
-    if (data.length === 0) {
-      res.status(404).json({ message: "No data found for EURUSD" });
-    } else {
-      res.status(200).json(data);
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 process.on("SIGINT", () => {
   client.close().then(() => {
